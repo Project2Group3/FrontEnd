6bc7be0b659dc6674e556f73f803e36b9afa5db9
LukeBerryPRO <lukeberrycs@gmail.com>
2024-10-08 10:15:34 -0700

    Added home page, routes to pages when home page buttons clicked

--- wishlist/src/App.js
+++ wishlist/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddItem from './pages/AddItem/AddItem.js';
+import CreateAccount from './pages/CreateAccount/CreateAccount.js';
+import EditItem from './pages/EditItem/EditItem.js';
+import EditUser from './pages/EditUser/EditUser.js';
+import Lists from './pages/Lists/Lists.js';
+import Login from './pages/Login/Login.js';
+import Home from './pages/Home/Home.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div class="page-top">
-        <h1>Home</h1>
+    <Router>
+      <div className="App">
+        <div className="page-top">
+          <h1>Home</h1>
+        </div>
+        <div className="nav-area">
+          <Routes>
+            <Route path="/AddItem" element={<AddItem />} />
+            <Route path="/CreateAccount" element={<CreateAccount />} />
+            <Route path="/EditItem" element={<EditItem />} />
+            <Route path="/EditUser" element={<EditUser />} />
+            <Route path="/Lists" element={<Lists />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-      <div class="nav-area">
-        <button>Create New Account</button>
-        <button>Login</button>
-      </div>
-    </div>
+    </Router>
   );
 }
 
